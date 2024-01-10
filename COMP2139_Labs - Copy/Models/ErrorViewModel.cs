36a40974b141b0ef284f96a74339e86f5ff34842
Sherry Zarei <SHERRY.ZAREI94@GMAIL.COM>
2024-01-10 11:11:44 -0500

    Initial project setup

---
+++ COMP2139_Labs - Copy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP2139_Labs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
