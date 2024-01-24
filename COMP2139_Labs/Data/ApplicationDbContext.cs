f49ffaf42b5d62d1ce771a2a2bfadc956f39fb4b
Sherry Zarei <SHERRY.ZAREI94@GMAIL.COM>
2024-01-24 10:35:40 -0500

    Week 3

---
+++ COMP2139_Labs/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using COMP2139_Labs.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP2139_Labs.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
