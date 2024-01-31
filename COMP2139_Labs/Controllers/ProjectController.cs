464d4e0f02019271134194eaec43f663e3a50240
Sherry Zarei <SHERRY.ZAREI94@GMAIL.COM>
2024-01-31 11:48:30 -0500

    Week 4

--- COMP2139_Labs/Controllers/ProjectController.cs
+++ COMP2139_Labs/Controllers/ProjectController.cs
@@ -1,28 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using COMP2139_Labs.Models;
+using COMP2139_Labs.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace COMP2139_Labs.Controllers
 {
     public class ProjectController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ProjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet] 
         public IActionResult Index() //Actions in controllers are only methods
         {
-            var projects = new List<Project>()
-            {
-                new Project { ProjectId = 1, Name = "Project 1", Description = "My First Project"}
-            };
+            var projects = _context.Projects.ToList();
             return View(projects);  //finds the view from the method Index()
         }
 
         [HttpGet]
         public IActionResult Details(int id)
         {
-            var project = new Project { ProjectId = id, Name = "Project " + id, Description = "Details of project " + id };
+            var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
             return View(project);
         }
 
         [HttpGet]
+        [ValidateAntiForgeryToken]
         public IActionResult Create()
         {
             return View();
@@ -31,7 +42,73 @@ namespace COMP2139_Labs.Controllers
         [HttpPost]
         public IActionResult Create(Project project)
         {
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _context.Projects.Add(project);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(project);  
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var project = _context.Projects.Find(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return View(project);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public  IActionResult Edit(int id, [Bind("ProjectId, Name, Description")]Project project)
+        {
+            if(id != project.ProjectId)
+            {
+                return NotFound();
+            }
+            
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(project);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProjectExists(project.ProjectId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(project);
+
+        }
+
+        public bool ProjectExists(int id)
+        {
+            return _context.Projects.Any(e => e.ProjectId == id);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id);
+            if(project == null){
+                return NotFound();
+            }
+            return View(project);
+        }
+
     }
 }
